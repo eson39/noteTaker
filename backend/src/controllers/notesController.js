a47b709380ae474baaee325408b39669c86b5f85
eson39 <eson39@gatech.edu>
2025-09-16 00:01:13 -0400

    defined endpoint definitions
    restapi methods are functional in controller

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,15 +1,47 @@
-export function getAllNotes(req, res) {
-    res.status(200).send("Note has been fetched.");
+import Note from "../models/Note.js"
+
+export async function getAllNotes(req, res) {
+    try {
+        const notes = await Note.find();
+        res.status(200).json(notes);
+    } catch(error) {
+        res.status(500).json({message: "Internal server error"});
+    }
 }
 
-export function createNote(req, res) {
-    res.status(201).json({message: "Note created successfully!"});
+export async function createNote(req, res) {
+    try {
+        const{title, content} = req.body;
+        const newNote = new Note({title:title, content: content});
+
+        await newNote.save();
+        res.status(201).json({message: "Note created successfully!"});
+    } catch(error) {
+        res.status(500).json({message: "Internal server error."});
+    }
 }
 
-export function updateNote(req, res) {
-    res.status(200).json({message: "Note updated successfully!"});
+export async function updateNote(req, res) {
+    try {
+        const {title, content} = req.body;
+        const updatedNote = await Note.findByIdAndUpdate(req.params.id, {title, content}, {new: true,});
+        if (!updatedNote) {
+            return res.status(404).json({message: "Note not found!"});
+        }
+        res.status(200).json({message: "Note updated successfully!"});
+    } catch (error) {
+        res.status(500).json({message: "Internal server error."});
+    }
 }
 
-export function deleteNote (req, res) {
-    res.status(200).json({message: "Note deleted successfully!"});
+export async function deleteNote (req, res) {
+    try {
+        const deletedNote = await Note.findByIdAndDelete(req.params.id, {title, content}, {new: true});
+        if (!deletedNote) {
+            res.status(404).json({message: "Node not found!"});
+        }
+        res.status(200).json({message: "Note deleted successfully!"});
+    } catch (error) {
+        res.status(500).json({message: "Internal server error."});
+    }
 }
