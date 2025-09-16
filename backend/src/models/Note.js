2d5919037745665bccbc6ef738eecfa7bb747ffe
eson39 <eson39@gatech.edu>
2025-09-15 23:25:01 -0400

    schema for note

--- backend/src/models/Note.js
+++ backend/src/models/Note.js
@@ -1,2 +1,19 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, mongo } from "mongoose";
 
+const noteSchema = new mongoose.Schema(
+    {
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    },
+    {timestamps: true}
+);
+
+const Note = mongoose.model("Note", noteSchema);
+
+export default Note;
