96f6b8506ad63da16cbf53316256cb89b1c84199
eson39 <eson39@gatech.edu>
2025-09-16 12:31:52 -0400

    added rl
    added rate limiter and made database connection first before listening to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,21 +2,24 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
+import rateLimiter from "./config/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
 app.use(express.json());
+app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-    console.log("Server started on PORT:", PORT);
-});
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server started on PORT:", PORT);
+    });
+})
+
 
 
 
